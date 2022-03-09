c9c126ebc064a294de17814e5e0e282e137e6462
jenhakk <75015030+jenhakk@users.noreply.github.com>
2022-03-09 13:49:44 +0200

    update

--- src/RoboRun.java
+++ src/RoboRun.java
@@ -1,41 +1,54 @@
 import java.io.File;
-
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.utility.Delay;
-import lejos.utility.Stopwatch;
 
 /**
- * Robot's main class and main thread
+ * Date: Mar 08-2022 This is an application for Lego Mindstorms EV3 robot that
+ * follows line by reading light values using color sensor and detects obstacles
+ * using ultrasonic sensor. It has been built using two extra threads and a
+ * class that transfers data between threads. It plays .wav file in the
+ * beginning of the app.
+ * 
+ * @author jenna hakkarainen, amanda karjalainen, anna-maria palm
+ * @version 1.0
  *
  */
 public class RoboRun {
+
+	/**
+	 * Main method of this application
+	 * 
+	 * @param args array of String arguments
+	 */
 
 	public static void main(String[] args) {
 
 		DataTransfer DT = new DataTransfer();
 		RobotMoves RMObj = new RobotMoves(DT);
 		ObstacleDetector ODObj = new ObstacleDetector(DT);
-		SoundLib SD = new SoundLib(DT);
-		
+
+		File beginning = new File("Beginning.wav");
+
+		// Wait Enter to be pushed and delay 5 seconds
 		System.out.println("Press Enter to start");
 		Button.ENTER.waitForPress();
-
 		Delay.msDelay(5000);
 
+		// Create and start threads
 		Thread Moves = new Thread(RMObj);
 		Thread Detect = new Thread(ODObj);
-		Thread Sounds = new Thread(SD);
 
-		Sounds.start();
 		Moves.start();
 		Detect.start();
-		
 
-//		while (Button.getButtons() != 0) {
-//			break;
-//
-//		}
+		//Play sound in the beginning of the program
+		Sound.playSample(beginning);
+
+		while (Button.getButtons() != 0) {
+			break;
+
+		}
 
 	}
 
--- src/RobotMoves.java
+++ src/RobotMoves.java
@@ -1,7 +1,4 @@
-import java.io.File;
 
-import lejos.hardware.Button;
-import lejos.hardware.Sound;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
@@ -11,173 +8,168 @@ import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.MovePilot;
-import lejos.utility.Delay;
-import lejos.utility.Stopwatch;
 
+/**
+ * Class where robot follows the line and goes around the detected obstacle.
+ *
+ */
 public class RobotMoves implements Runnable {
 
+	private DataTransfer DTObj;
+
+	// Sensor and motors
 	private static EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
 	protected static EV3LargeRegulatedMotor motorR = new EV3LargeRegulatedMotor(MotorPort.A);
 	protected static EV3LargeRegulatedMotor motorL = new EV3LargeRegulatedMotor(MotorPort.B);
 
+	// Wheels for chassis and chassis for move pilot
 	Wheel left = WheeledChassis.modelWheel(motorL, 55).offset(50);
 	Wheel right = WheeledChassis.modelWheel(motorR, 55).offset(-50);
-
 	Chassis chassis = new WheeledChassis(new Wheel[] { left, right }, WheeledChassis.TYPE_DIFFERENTIAL);
-
 	MovePilot pilot = new MovePilot(chassis);
-	Stopwatch timer = new Stopwatch();
 
+	/**
+	 * float list for color sensor values
+	 */
 	private static float[] sample;
-	private DataTransfer DTObj;
+	/**
+	 * double value for line limit
+	 */
 	private static double tooBlack = 5.5;
+	/**
+	 * double value for line limit
+	 */
 	private static double tooWhite = 26;
 	// outer line speed 245, inner line speed 230
+	/**
+	 * int value for faster motor speed
+	 */
 	private static int fastest = 245;
+	/**
+	 * int value for encountered obstacles
+	 */
 	public static int round = 0;
-	private static int time = 0;
 
+	/**
+	 * Constructor of the class
+	 * 
+	 * @param DT for bringing DataTransfer object to class
+	 */
 	public RobotMoves(DataTransfer DT) {
 		this.DTObj = DT;
 
 	}
 
-	// Timer tsekkaus
-	// Jos eksyy viivalta muulloin kuin esteen ohituksen j‰lkeen niin miten k‰y?!
-	// ƒ‰nien etsint‰ ja koodaus
-	// Sis‰puolen rata, miten toimii?
-	// ohjelman pys‰ytys nappulasta
-
+	/**
+	 * Run method of the class
+	 */
 	@Override
 	public void run() {
+
+		// settings for color sensor
 		cs.setFloodlight(Color.RED);
 		cs.setFloodlight(true);
 		cs.setCurrentMode("Red");
 
-		// value 0 is the darkest, 100 the lightest
 		sample = new float[cs.sampleSize()];
 
 		while (true) {
-			// timer starts
-			// timer.reset();
 
-			SoundLib.locked = false;
-			
-			if (DTObj.isStarted() == true) {
+			// Status 1 = moving
+			// Normal line following
 
-				// 1. ALOITUS JA 6. ELI KUN TAKAISIN RADALLA
-				// Status 1 = moving
-				// Normal line following
-				if (DTObj.getStatus() == 1 && DTObj.isObstacleDetected() == false) {
+			// 1.1 If first condition was not executed, check if robot is moving and has
+			// obstacle not been detected
+			if (DTObj.getStatus() == 1 && DTObj.isObstacleDetected() == false) {
 
-					// Starts on outer line
+				// Starts on outer line
 
-					// When robot is on the edge of line 50/50 and moves too much to the white,
-					// return left
-					if (getRed() >= tooWhite) {
+				// 1.1.1 When robot is on the edge of line 50/50 and moves too much to the
+				// white,
+				// move back to left
+				if (getRed() >= tooWhite) {
 
-						// outer line -> return left motorL.setSpeed(145), motorR.setSpeed(fastest)
-						// inner line -> return right motorR.setSpeed(130), motorL.setSpeed(fastest)
-						motorL.setSpeed(145);
-						motorR.setSpeed(fastest);
-						motorL.forward();
-						motorR.forward();
+					// outer line -> return left motorL.setSpeed(145), motorR.setSpeed(fastest)
+					// inner line -> return right motorR.setSpeed(130), motorL.setSpeed(fastest)
+					motorL.setSpeed(145);
+					motorR.setSpeed(fastest);
+					motorL.forward();
+					motorR.forward();
 
-						// When robot is on the edge of line 50/50 and moves too much to the black,
-						// return right
-					} else if (getRed() <= tooBlack) {
+					// 1.1.2 When robot is on the edge of line 50/50 and moves too much to the
+					// black,
+					// move back to right
+				} else if (getRed() <= tooBlack) {
 
-						// outer line -> return right motorR.setSpeed(145), motorL.setSpeed(fastest)
-						// inner line -> return left motorL.setSpeed(130), motorR.setSpeed(fastest)
-						motorR.setSpeed(145);
-						motorL.setSpeed(fastest);
-						motorR.forward();
-						motorL.forward();
+					// outer line -> return right motorR.setSpeed(145), motorL.setSpeed(fastest)
+					// inner line -> return left motorL.setSpeed(130), motorR.setSpeed(fastest)
+					motorR.setSpeed(145);
+					motorL.setSpeed(fastest);
+					motorR.forward();
+					motorL.forward();
 
-					} else {
-						motorR.setSpeed(fastest);
-						motorL.setSpeed(fastest);
-						motorR.forward();
-						motorL.forward();
-					}
-					// 2. ESTE HAVAITTU
-					// Jos status on 0 ja este havaittu, kierr‰ este
-					// If status = 0 (stop) and obstacle is detected -> go around the obstacle
-				} else if (DTObj.getStatus() == 0 && DTObj.isObstacleDetected() == true) {
+					// 1.1.3. Otherwise move forward
+				} else {
+					motorR.setSpeed(fastest);
+					motorL.setSpeed(fastest);
+					motorR.forward();
+					motorL.forward();
+				}
 
-					// 3. SIIRRYTƒƒN METODIIN clearObstacle
-					if (round == 0) {
-						SoundLib.locked = false;
-						clearObstacle();
-						round++;
-						// }
+				// 1.2 Else if first or second condition aren't executed, check if robot isn't
+				// moving and obstacle is detected
+			} else if (DTObj.getStatus() == 0 && DTObj.isObstacleDetected() == true) {
 
-					} else {
-						motorR.stop();
-						motorL.stop();
-						// Sound.playSample(ending, 100);
-						// Timer stops and prints the time
-//					time = timer.elapsed();
-//					System.out.println("Time: " + time/10.0F + " seconds");
-//					Delay.msDelay(5000);
-						System.exit(0);
+				// 1.2.1 Check if round is zero (obstacle has not been passed yet) and then
+				// clear the obstacle.
+				if (round == 0) {
+					clearObstacle();
+					round++;
 
-					}
-
-					// 4. KATSOTAAN METODILLA OLLAANKO VIIVALLA
-					detectLine();
-					// System.out.println(detectLine());
-					// System.out.println(getRed());
-
-					// Jos viiva havaittu, k‰‰nn‰ oikealle
-					// If line is detected -> make a turn right
-					if (DTObj.isLineDetected() == true) {
-
-						System.out.println("line detected true");
-						pilot.setAngularSpeed(60);
-						pilot.rotate(-30);
-						searchLine();
-
-						// 5. VAAN HYPƒTTIIN SUORAAN TƒHƒN -> searchLine()
-
-						// If line is not detected, search line with searcLine()
-					} else {
-						System.out.println("ollaanko searchissa?");
-						searchLine();
-					}
-
+					// 1.2.2 round is more than 0, stop motors and exit system.
 				} else {
 
 					motorR.stop();
 					motorL.stop();
-				}
-			}
-			while (Button.getButtons() != 0) {
-				break;
+					System.exit(0);
 
+				}
+
+				detectLine();
+
+				// 2.1 After the arc has been made and line is detected, make a turn right to
+				// correct pose
+				if (DTObj.isLineDetected() == true) {
+
+					System.out.println("line detected true");
+					pilot.setAngularSpeed(60);
+					pilot.rotate(-30);
+
+					// start searching the line
+					searchLine();
+
+					// 2.2 else (line is not detected, search line with searcLine())
+				} else {
+
+					searchLine();
+
+				}
+
+				// 1.3 Stop motors
+			} else {
+
+				motorR.stop();
+				motorL.stop();
 			}
 		}
 
 	}
 
 	/**
-	 * Method that gets light values in to list, changes them to integers (*100) and
-	 * returns the first index from the list.
-	 *
-	 */
-//	public static int getRed() {
-//
-//		cs.fetchSample(sample, 0);
-//		int value = (int) (sample[0] * 100);
-//
-//		return value;
-//
-//	}
-
-	/**
-	 * Method that gets light values in to list, changes them to doubles (*100) and
-	 * returns the first index from the list.
-	 *
+	 * Method that gets light values in to list, changes them to doubles (*100).
+	 * Value 0 is the darkest, 100 the lightest.
+	 * 
+	 * @return returns the first index from the list
 	 */
 	public static double getRed() {
 
@@ -189,9 +181,10 @@ public class RobotMoves implements Runnable {
 	}
 
 	/**
-	 * Method that checks if values from color sensor are <= than tooBlack values
-	 * and changes lineDetected to boolean, returns detectLine value
-	 *
+	 * Method that checks if values from color sensor are less or equal than
+	 * tooBlack (5.5) values and changes lineDetected to boolean value
+	 * 
+	 * @return returns as a boolean value if the line has been detected
 	 */
 	public boolean detectLine() {
 		boolean detectLine = false;
@@ -210,50 +203,29 @@ public class RobotMoves implements Runnable {
 	}
 
 	/**
-	 * Method that executes an arc around the obstacle. If detectLine-method returns
-	 * false, robot makes a turn to the right and then an arc. If line is detected
-	 * in the middle of arc, robot stops. ObstacleDetected is set to false.
+	 * Method that executes an arc around the obstacle. If detectLine() returns
+	 * false, robot makes a turn to the right and then the arc. If line is detected
+	 * in the middle of the arc, robot stops. ObstacleDetected is set to false.
 	 */
-
 	public void clearObstacle() {
-		// Tehd‰‰n kaari ja asetetaan obstacleDetected > false
 
 		while (detectLine() == false) {
-			// ulkoreuna
 
-			//pilotti stoppaa ensin ‰‰nen takia
-			//pilot.stop();
+			// OUTER LINE
 			pilot.setAngularSpeed(60);
 			pilot.rotate(-60);
-			// Delay.msDelay(1500);
 			pilot.setLinearSpeed(95);
 			pilot.arc(300, 200, true);
+
+			// Check if line is detected while making the arc, if true pilot stops.
 			while (pilot.isMoving()) {
 				if (detectLine() == true) {
 					pilot.stop();
-					System.out.println("stop");
 				}
 			}
 
 			DTObj.setObstacleDetected(false);
-			// Sound.playSample(passed, 100);
 
-			// break;
-
-			// INNER LINE
-//			pilot.setAngularSpeed(60);
-//			pilot.rotate(60);
-//			// Delay.msDelay(1500);
-//			pilot.setLinearSpeed(95);
-//			pilot.arc(-300, 200, true);
-//			while (pilot.isMoving()) {
-//				if (detectLine() == true) {
-//					pilot.stop();
-//					System.out.println("stop");
-//				}
-//			}
-//
-//			DTObj.setObstacleDetected(false);
 		}
 	}
 
@@ -263,13 +235,9 @@ public class RobotMoves implements Runnable {
 	 * to following the line. If it returns false: pilot is used to make a waving
 	 * move, first to the right and then to left. If detectLine() returns true (line
 	 * is found), robot stops and breaks the loop.
-	 *
 	 */
-	// 5. JATKUU: JOS detectLine() ILMOITTAA ETTƒ OLLAAN VIIVALLA, MENNƒƒN EKAAN
-	// IFIIN JA PƒƒSTƒƒN TAKAISIN VIIVANSEURAUKSEEN
 	public void searchLine() {
-		// niin kauan kun detectLine() antaa arvon false eli getRed() on >= kuin 6
-		// tehd‰‰n k‰‰ntymisliikett‰
+
 		while (true) {
 
 			detectLine();
@@ -294,7 +262,6 @@ public class RobotMoves implements Runnable {
 					}
 				}
 
-				// pilot.setLinearSpeed(70);
 				pilot.travelArc(-100, 150, true);
 
 				while (pilot.isMoving()) {
@@ -308,4 +275,5 @@ public class RobotMoves implements Runnable {
 			}
 		}
 	}
+
 }
